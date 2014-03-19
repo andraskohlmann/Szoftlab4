2269ce080f7574394017b3d04f3191d5d6c10996
zscseh <zscseh93@gmail.com>
2014-03-19 21:03:47 +0100

    Put Rune sequence edited
    Game

--- src/model/Game.java
+++ src/model/Game.java
@@ -18,26 +18,27 @@ import control.SkeletonUI;
 //
 
 public class Game {
-	
+
 	private Map map;
-	
+
 	public void Skeleton_MapSetter(Map m) {
 		map = m;
 	}
-	
+
 	public void tick() {
-//		SkeletonUI.enterFunction(this, "tick");
-//		
-//		Ticker t = new Ticker();
-//		SkeletonUI.addObject(t, "ticker", true);
-//		
-//		List<EnemyUnit> l = new ArrayList<EnemyUnit>();
-//		SkeletonUI.addObject(l, "enemylist", false);
-//		
-//		if(SkeletonUI.stringQuestion("Hogy dugnád (Orálisan, Análisan, Vaginálisan)?", "O", "a", "v").equals("V")) 
-//			t.remove(l);
-//		
-//		SkeletonUI.leaveFunction();
+		// SkeletonUI.enterFunction(this, "tick");
+		//
+		// Ticker t = new Ticker();
+		// SkeletonUI.addObject(t, "ticker", true);
+		//
+		// List<EnemyUnit> l = new ArrayList<EnemyUnit>();
+		// SkeletonUI.addObject(l, "enemylist", false);
+		//
+		// if(SkeletonUI.stringQuestion("Hogy dugnád (Orálisan, Análisan, Vaginálisan)?",
+		// "O", "a", "v").equals("V"))
+		// t.remove(l);
+		//
+		// SkeletonUI.leaveFunction();
 	}
 
 	public void notifyIfDead(EnemyUnit e) {
@@ -45,27 +46,31 @@ public class Game {
 
 		SkeletonUI.leaveFunction();
 	}
-	
+
 	public void Skeleton_MouseOnTile_Rune() {
 		boolean available = map.checkRune(0, 0);
-		
-//		if(available) {
-//			
-//		}
+
+		if (available) {
+
+			Rune rune = new Rune();
+			SkeletonUI.addObject(rune, "rune", true);
+
+			map.putRune(0, 0, rune);
+		}
 	}
+
 	public void Skeleton_MouseOnTile_Swamp() {
-		boolean available =  map.checkSwamp(0, 0);
+		boolean available = map.checkSwamp(0, 0);
 		if (available) {
 			Rune r = new Rune();
 			SkeletonUI.addObject(r, "r", true);
-			
+
 			Swamp s = new Swamp();
 			SkeletonUI.addObject(s, "s", true);
-			
-			map.putSwamp(0, 0,s);
-			
+
+			map.putSwamp(0, 0, s);
+
 		}
-	
-		
+
 	}
 }
