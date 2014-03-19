68d52ff8c351681a97acd63d6b9ec824130360a8
zscseh <zscseh93@gmail.com>
2014-03-19 19:06:10 +0100

    Started Put Rune sequence
    SkeletonManager PutRune created

--- src/control/SkeletonManager.java
+++ src/control/SkeletonManager.java
@@ -5,6 +5,8 @@ import model.Ticker;
 import model.enemies.EnemyUnit;
 import model.friendly.Swamp;
 import model.friendly.Tower;
+import model.mapitem.Field;
+import model.mapitem.Map;
 import model.mapitem.Road;
 import model.runes.Rune;
 
@@ -37,6 +39,32 @@ public class SkeletonManager {
 		
 		ticker.tick();	
 		
+	}
+	
+	public static void PutRune() {
+		
+		Game g = new Game();
+		SkeletonUI.addObject(g, "g", true);
+		
+		Map m = new Map();
+		SkeletonUI.addObject(m, "m", true);
+		
+		Field f = new Field();
+		SkeletonUI.addObject(f, "f", true);
+		
+		Road r = new Road();
+		SkeletonUI.addObject(r, "r", true);
+		
+		Tower t = new Tower();
+		SkeletonUI.addObject(t, "t", true);
+		
+		Swamp s = new Swamp();
+		SkeletonUI.addObject(s, "s", true);
+		
+		String answer = SkeletonUI.stringQuestion("Az adott Tile Field vagy Swamp?", "F", "S");
+		
+		if(answer == "F")
+			g.
 	}
 
 }
