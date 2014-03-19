a61f1b176933060a9f8cc593a77ca00ecb909235
Martinek Vilmos <vilmos.martinek@gmail.com>
2014-03-19 17:15:20 +0100

    Updated SkeletonManager
    More convenient, also added helping javadoc comments.

--- src/control/SkeletonManager.java
+++ src/control/SkeletonManager.java
@@ -22,57 +22,87 @@ public class SkeletonManager {
 
 	}
 
+	/**
+	 * A függvény ami a létrehozott objektumokat regisztrálja be a nevükkel.
+	 * 
+	 * @param object
+	 *            - az új objektum
+	 * @param name
+	 *            - a neve
+	 * @param loud
+	 *            - true, ha a létrehozásnak látszania kell a
+	 *            szekvenciadiagramon, false, ha nem
+	 */
 	public static void addObject(Object object, String name, boolean loud) {
-		objects.put(object, "\"" + name +  "\"");
-		
+		objects.put(object, "\"" + name + "\"");
+
 		if (loud) {
 			indent();
 			System.out.println("[CREATED] " + writeObject(object));
 		}
 	}
 
+	/**
+	 * A függvények belépési pontján kell hívni.
+	 * 
+	 * @param who
+	 *            - ide csak azt írd hogy this
+	 * @param function
+	 *            - a függvény neve, ahonnan hívod
+	 * @param params
+	 *            - írd le a függvény paramétereit egymás után sorba
+	 */
 	public static void enterFunction(Object who, String function,
-			Object[] params) {
-		
+			Object... params) {
+
 		indent();
 		System.out.print("[ENTERED] \"" + function + "\" function of "
 				+ writeObject(who));
 
-		if (params != null) {
+		if (params.length > 0) {
 			System.out.println(", with the following parameters:");
-			
+
 			indentation++;
-			
+
 			for (Object p : params) {
 				indent();
 				System.out.println(writeObject(p));
 			}
-		}
-		else {
+		} else {
 			indentation++;
 			System.out.println();
 		}
 	}
 
+	/**
+	 * A függvény kilépési pontján kell hívni.
+	 */
+	public static void leaveFunction() {
+		indentation--;
+
+		indent();
+		System.out.println("[LEFT]");
+	}
+
+	/**
+	 * A függvény kilépési pontján kell hívni.
+	 * 
+	 * @param ret
+	 *            - a visszatérési érték
+	 */
 	public static void leaveFunction(Object ret) {
 		indentation--;
-		
+
 		indent();
-		System.out.print("[LEFT]");
-		
-		if (ret != null) {
-			System.out.println(" with the returning value of: " + writeObject(ret));
-		}
-		else {
-			System.out.println();
-		}
+		System.out.println("[LEFT] with the returning value of: "
+				+ writeObject(ret));
 	}
 
 	private static void indent() {
 		for (int i = 0; i < indentation; i++)
 			System.out.print("   ");
 	}
-	
+
 	private static String writeObject(Object o) {
 		return objects.get(o) + " : " + o.getClass().getSimpleName();
 	}
