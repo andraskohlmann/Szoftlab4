40607b46a9980558370f52cf1a83f279dd71ef7b
matyi25 <t.matyi@tomolya.net>
2014-04-22 03:24:58 +0200

    Field

--- src/model/mapitem/Field.java
+++ src/model/mapitem/Field.java
@@ -46,11 +46,14 @@ public class Field extends Tile {
 		super.removeFog(f);
 	}
 
-	public boolean hasTower() {
-		if (tower != null)
-			return true;
-		else
-			return false;
-
+	public boolean hasTower()
+	{
+		if (tower!= null) return true;
+		else return false;
+		
+	}
+	public Tower Prototype_getTower()
+	{
+		return tower;
 	}
 }
