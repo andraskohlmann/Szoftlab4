4f7dcf6a875de4db06f546e8ab11b2cbe99b0cf5
Martinek Vilmos <vilmos.martinek@gmail.com>
2014-05-07 21:04:23 +0200

    ViewFactory updated

--- src/view/ViewFactory.java
+++ src/view/ViewFactory.java
@@ -1,11 +1,24 @@
 package view;
 
+import model.mapitem.Field;
+import model.mapitem.FinishedRoad;
+import model.mapitem.Road;
+import view.mapitem.FieldView;
+import view.mapitem.FinishedRoadView;
 import view.mapitem.RoadView;
 
 public class ViewFactory {
 	
-	public static ViewBase createView(Object o){
-		return new RoadView();
+	public static ViewBase createView(Road r){
+		return new RoadView(r);
+	}
+	
+	public static ViewBase createView(Field f){
+		return new FieldView(f);
+	}
+	
+	public static ViewBase createView(FinishedRoad f){
+		return new FinishedRoadView();
 	}
 
 }
