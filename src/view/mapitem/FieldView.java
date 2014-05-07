1490ce5d44d0336ed99605a7657cdcdabe689fbd
zscseh <zscseh93@gmail.com>
2014-05-07 20:31:29 +0200

    FieldView

--- src/view/mapitem/FieldView.java
+++ src/view/mapitem/FieldView.java
@@ -1,10 +1,17 @@
 package view.mapitem;
 import java.awt.Graphics;
 
+import model.mapitem.Field;
 import view.View;
 import view.ViewBase;
+import view.friendly.TowerView;
 
 public class FieldView extends ViewBase implements View {
+	
+	private Field field;
+	private TowerView towerview;
+	private FogView fogview;
+	
 	public void notifyView() {
 	
 	}
