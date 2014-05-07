8f1b93b911bb80b03a7e01dc027f6449abc79666
m2kohand <uh.liamg@gmail.com>
2014-05-07 21:17:54 +0200

    TowerView, RuneView

--- src/view/friendly/TowerView.java
+++ src/view/friendly/TowerView.java
@@ -1,4 +1,5 @@
 package view.friendly;
+import java.awt.Color;
 import java.awt.Graphics;
 
 import model.friendly.Tower;
@@ -11,6 +12,8 @@ public class TowerView extends ViewBase {
 	private RuneView runeView;
 	
 	public void draw(Graphics g) {
+		g.setColor(Color.GRAY);
+		g.fillOval(x + 2, y + 2, 16, 16);
 		runeView.draw(g);
 	}
 }
