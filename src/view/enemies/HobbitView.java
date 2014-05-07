2a1f3744c18eac1af14ed791e318d9184867f7c2
galaibence <bence@baja.hu>
2014-05-07 20:34:10 +0200

    Hobbit and Man getParameters

--- src/view/enemies/HobbitView.java
+++ src/view/enemies/HobbitView.java
@@ -5,7 +5,10 @@ import java.awt.Graphics;
 import view.ViewBase;
 
 public class HobbitView extends ViewBase {
+	
+	private Hobbit hobbit;
+	
 	public void draw(Graphics g) {
-
+		int health = hobbit.getParameters();
 	}
 }
--- src/view/enemies/ManView.java
+++ src/view/enemies/ManView.java
@@ -2,10 +2,14 @@ package view.enemies;
 
 import java.awt.Graphics;
 
+import model.enemies.Man;
 import view.ViewBase;
 
 public class ManView extends ViewBase {
+	
+	private Man man;
+	
 	public void draw(Graphics g) {
-
+		int health = man.getParameters();
 	}
 }
