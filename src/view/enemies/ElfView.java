d771637bbaa33453a7543726f4db4af177e0a699
zscseh <zscseh93@gmail.com>
2014-05-07 21:20:21 +0200

    ElfView; ManView

--- src/view/enemies/ElfView.java
+++ src/view/enemies/ElfView.java
@@ -3,6 +3,7 @@ package view.enemies;
 import java.awt.Color;
 import java.awt.Graphics;
 
+import model.Common;
 import model.enemies.Elf;
 import view.ViewBase;
 
@@ -16,7 +17,7 @@ public class ElfView extends ViewBase {
 		g.setColor(Color.WHITE);
 		g.fillOval(x, y, 10, 10);
 		g.setColor(Color.RED);
-		
+		g.fillRect(x, y-5, (int) (10*(double)health/Common.lifeElf), 3);
 		g.setColor(Color.BLACK);
 		g.drawRect(x, y - 5, 10, 3);
 	}
--- src/view/enemies/ManView.java
+++ src/view/enemies/ManView.java
@@ -1,7 +1,9 @@
 package view.enemies;
 
+import java.awt.Color;
 import java.awt.Graphics;
 
+import model.Common;
 import model.enemies.Man;
 import view.ViewBase;
 
@@ -11,5 +13,12 @@ public class ManView extends ViewBase {
 	
 	public void draw(Graphics g) {
 		int health = man.getParameters();
+		
+		g.setColor(Color.PINK);
+		g.fillOval(x, y, 10, 10);
+		g.setColor(Color.RED);
+		g.fillRect(x, y-5, (int) (10*(double)health/Common.lifeMan), 3);
+		g.setColor(Color.BLACK);
+		g.drawRect(x, y - 5, 10, 3);
 	}
 }
