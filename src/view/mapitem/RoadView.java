3e1b7be3d80cec524562b7561464590f5f4b82be
galaibence <bence@baja.hu>
2014-05-07 20:36:55 +0200

    RoadView again o.O

--- src/view/mapitem/RoadView.java
+++ src/view/mapitem/RoadView.java
@@ -1,16 +1,29 @@
 package view.mapitem;
 import java.awt.Graphics;
+import java.util.List;
 
+import model.mapitem.Road;
 import view.View;
 import view.ViewBase;
+import view.friendly.SwampView;
 
 public class RoadView extends ViewBase implements View {
 	
+	private Road road;
+	private SwampView swampView;
+	private List<ViewBase> enemyUnitViews; 
+
 	public void notifyView() {
 	
 	}
 	
 	public void draw(Graphics g) {
-	
+		if (needToRepaint) {
+			if (swampView != null)
+				swampView.draw(g);
+			for(ViewBase euv : enemyUnitViews) {
+				euv.draw(g);
+			}
+		}
 	}
 }
