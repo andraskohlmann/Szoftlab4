05eacf0a5d0b7c09a507011fcda9beedc1ff678f
zscseh <zscseh93@gmail.com>
2014-05-07 19:57:01 +0200

    view.mapitem

---
+++ src/view/mapitem/FinishedRoadView.java
@@ -0,0 +1,15 @@
+package view.mapitem;
+import java.awt.Graphics;
+
+import view.View;
+import view.ViewBase;
+
+public class FinishedRoadView extends ViewBase implements View {
+	public void notifyView() {
+	
+	}
+	
+	public void draw(Graphics g) {
+	
+	}
+}
