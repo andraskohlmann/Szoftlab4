2921235b6f34d2e81f22cb40eedbd50645d6760b
m2kohand <uh.liamg@gmail.com>
2014-05-07 21:23:56 +0200

    Lot of RuneViews

--- src/view/runes/HobbitRuneView.java
+++ src/view/runes/HobbitRuneView.java
@@ -1,10 +1,12 @@
 package view.runes;
+import java.awt.Color;
 import java.awt.Graphics;
 
 import view.ViewBase;
 
 public class HobbitRuneView extends ViewBase {
 	public void draw(Graphics g) {
-	
+		g.setColor(Color.ORANGE);
+		g.fillPolygon(new int[]{x + 10, x + 5, x + 15, x + 10} ,new int[]{y + 5, y + 10, y + 10, y + 15} , 4);
 	}
 }
