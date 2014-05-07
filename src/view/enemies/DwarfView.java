e33a1ed7f837581883b04bf962d254f0e2fef0e6
galaibence <bence@baja.hu>
2014-05-07 21:18:33 +0200

    dwarf, hobbit draw method

--- src/view/enemies/DwarfView.java
+++ src/view/enemies/DwarfView.java
@@ -1,6 +1,8 @@
 package view.enemies;
+import java.awt.Color;
 import java.awt.Graphics;
 
+import model.Common;
 import model.enemies.Dwarf;
 import view.ViewBase;
 
@@ -10,5 +12,12 @@ public class DwarfView extends ViewBase {
 	
 	public void draw(Graphics g) {
 		int health = dwarf.getParameters();
+		
+		g.setColor(Color.BLUE);
+		g.fillOval(x, y, 10, 10);
+		g.setColor(Color.RED);
+		g.fillRect(x, y-5, (int) (10*(double)health/Common.lifeDwarf), 3);
+		g.setColor(Color.BLACK);
+		g.drawRect(x, y-5, 10, 3);
 	}
 }
--- src/view/enemies/HobbitView.java
+++ src/view/enemies/HobbitView.java
@@ -1,7 +1,9 @@
 package view.enemies;
 
+import java.awt.Color;
 import java.awt.Graphics;
 
+import model.Common;
 import model.enemies.Hobbit;
 import view.ViewBase;
 
@@ -11,5 +13,12 @@ public class HobbitView extends ViewBase {
 	
 	public void draw(Graphics g) {
 		int health = hobbit.getParameters();
+		
+		g.setColor(Color.CYAN);
+		g.fillOval(x, y, 10, 10);
+		g.setColor(Color.RED);
+		g.fillRect(x, y-5, (int) (10*(double)health/Common.lifeHobbit), 3);
+		g.setColor(Color.BLACK);
+		g.drawRect(x, y-5, 10, 3);
 	}
 }
