4d19857dc4598724ca7ffe592ee833d14972df1d
zscseh <zscseh93@gmail.com>
2014-05-07 20:41:18 +0200

    TowerView

--- src/view/friendly/TowerView.java
+++ src/view/friendly/TowerView.java
@@ -1,10 +1,16 @@
 package view.friendly;
 import java.awt.Graphics;
 
+import model.friendly.Tower;
 import view.ViewBase;
+import view.runes.RuneView;
 
 public class TowerView extends ViewBase {
-	public void draw(Graphics g) {
 	
+	private Tower tower;
+	private RuneView runeView;
+	
+	public void draw(Graphics g) {
+		runeView.draw(g);
 	}
 }
