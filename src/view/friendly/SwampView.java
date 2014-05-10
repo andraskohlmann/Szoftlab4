2c519cf4c498beb73bdd43d9e2d84d6bef61276c
matyi25 <t.matyi@tomolya.net>
2014-05-11 00:34:03 +0200

    view.friendly comments ready

--- src/view/friendly/SwampView.java
+++ src/view/friendly/SwampView.java
@@ -6,18 +6,43 @@ import java.awt.Graphics;
 import model.friendly.Swamp;
 import view.RelativeViewBase;
 
+/**
+ * 
+ * A játékban megjelenõ mocsarak kirajzolásáért felelõs objektum,leszármazik a
+ * RelativeViewBase osztályból.
+ * 
+ */
 public class SwampView implements RelativeViewBase {
-
+	/**
+	 * A kirajzolandó objektum tárolása
+	 */
 	private Swamp swamp;
 
+	/**
+	 * Konstruktor
+	 * 
+	 * @param s
+	 *            Swamp
+	 */
 	public SwampView(Swamp s) {
 		swamp = s;
 	}
 
+	/**
+	 * A mocsarat kirajzoló függvény. A helyes koordintákra kirajzol, valamint a
+	 * mocsárra lehelyezett Rune kirajzolását megvalósító függvényt is meghívja.
+	 * 
+	 * @param g
+	 *            Graphics
+	 * @param x
+	 *            int - x koordináta
+	 * @param y
+	 *            int - y koordináta
+	 */
 	public void draw(Graphics g, int x, int y) {
 		g.setColor(Color.MAGENTA);
 		g.fillRect(x + 2, y + 2, 16, 16);
-		
-		((RelativeViewBase)swamp.getRune().getView()).draw(g, x, y);
+
+		((RelativeViewBase) swamp.getRune().getView()).draw(g, x, y);
 	}
 }
--- src/view/friendly/TowerView.java
+++ src/view/friendly/TowerView.java
@@ -5,15 +5,37 @@ import java.awt.Graphics;
 
 import model.friendly.Tower;
 import view.RelativeViewBase;
-
+/**
+ * 
+ * A játékban megjelenõ tornyok kirajzolásáért felelõs objektum,leszármazik a
+ * RelativeViewBase osztályból.
+ * 
+ */
 public class TowerView implements RelativeViewBase {
-
+	/**
+	 * A kirajzolandó objektum tárolása
+	 */
 	private Tower tower;
-
+	/**
+	 * Konstruktor
+	 * 
+	 * @param t
+	 *            Tower
+	 */
 	public TowerView(Tower t) {
 		tower = t;
 	}
-
+	/**
+	 * A tornyot kirajzoló függvény. A helyes koordintákra kirajzol, valamint a
+	 * toronyra lehelyezett Rune kirajzolását megvalósító függvényt is meghívja.
+	 * 
+	 * @param g
+	 *            Graphics
+	 * @param x
+	 *            int - x koordináta
+	 * @param y
+	 *            int - y koordináta
+	 */
 	public void draw(Graphics g, int x, int y) {
 		g.setColor(Color.GRAY);
 		g.fillOval(x + 2, y + 2, 16, 16);
