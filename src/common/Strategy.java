90f9507bb7e5e8c4b1960853e2079a79720b0bed
m2kohand <uh.liamg@gmail.com>
2014-05-11 21:04:15 +0200

    First Strategy, game is easypeasy

--- src/common/Strategy.java
+++ src/common/Strategy.java
@@ -11,6 +11,18 @@ public class Strategy implements ActionListener {
 	private Game game;
 	private int ticks = 0;
 
+	private static final int second = 50;
+	private static final int firstDelay = 5;
+	private static final int firstWave = firstDelay + 20;
+	private static final int secondDelay = firstWave + 10;
+	private static final int secondWave = secondDelay + 40;
+	private static final int thirdDelay = secondWave + 15;
+	private static final int thirdWave = thirdDelay + 60;
+
+	private static final int firstRep = 100;
+	private static final int secondrep = 50;
+	private static final int thirdrep = 20;
+
 	public Strategy(Game g) {
 		game = g;
 	}
@@ -19,11 +31,52 @@ public class Strategy implements ActionListener {
 	public void actionPerformed(ActionEvent arg0) {
 
 		ticks++;
-		
+
 		game.tick();
-	
-		if (ticks > 10 && ticks % 50 == 0) {
+
+		if (ticks > firstDelay * second && ticks < firstWave * second) {
+			firstVawe(ticks);
+		} else if (ticks > secondDelay * second && ticks < secondWave * second) {
+			secondVawe(ticks);
+		} else if (ticks > thirdDelay * second && ticks < thirdWave * second) {
+			thirdVawe(ticks);
+		}
+	}
+
+	private void thirdVawe(int ticks2) {
+		if (ticks % thirdrep == 0) {
+			addRandomunit();
+		}
+	}
+
+	private void secondVawe(int ticks2) {
+		if (ticks % secondrep == 0) {
+			addRandomunit();
+		}
+	}
+
+	private void firstVawe(int ticks2) {
+		if (ticks % firstRep == 0) {
+			addRandomunit();
+		}
+	}
+
+	private void addRandomunit() {
+		switch (Randomizer.randomInt(4)) {
+		case 0:
+			game.addUnit(EnemyType.dwarf);
+			break;
+		case 1:
 			game.addUnit(EnemyType.elf);
+			break;
+		case 2:
+			game.addUnit(EnemyType.hobbit);
+			break;
+		case 3:
+			game.addUnit(EnemyType.man);
+			break;
+		default:
+			break;
 		}
 	}
 }
