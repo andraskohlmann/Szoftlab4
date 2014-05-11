3053640ca31aa84b1a2d8d3357ee9993e5f2d833
zscseh <zscseh93@gmail.com>
2014-05-12 01:06:21 +0200

    menu

--- src/view/MenuPanel.java
+++ src/view/MenuPanel.java
@@ -1,7 +1,13 @@
 package view;
 
+import java.awt.Color;
 import java.awt.GridLayout;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
@@ -28,6 +34,8 @@ public class MenuPanel extends JPanel {
 	private JButton hobbitRuneButton;
 	private JButton rangeRuneButton;
 	private JButton reloadRuneButton;
+	
+	private BufferedImage towerButtonImage;
 
 	/**
 	 * Konstruktor, inicializálja a gombok mûködését,tulajdonságait.
@@ -37,43 +45,76 @@ public class MenuPanel extends JPanel {
 	 */
 	public MenuPanel(Control control) {
 		setLayout(new GridLayout(0, 1));
-        		
-		towerButton = new JButton("Tower");
+		setBackground(Color.BLACK);
+		
+		towerButton = new JButton(new ImageIcon("textures//towerbutton.png"));
+		towerButton.setBorderPainted(false);
+		towerButton.setContentAreaFilled(false);
+		towerButton.setFocusPainted(false);
+		towerButton.setOpaque(false);
 		towerButton.setActionCommand("tower");
 		towerButton.addActionListener(control);
 		add(towerButton);
 
-		swampButton = new JButton("Swamp");
+		swampButton = new JButton(new ImageIcon("textures//swampbutton.png"));
+		swampButton.setBorderPainted(false);
+		swampButton.setContentAreaFilled(false);
+		swampButton.setFocusPainted(false);
+		swampButton.setOpaque(false);
 		swampButton.setActionCommand("swamp");
 		swampButton.addActionListener(control);
 		add(swampButton);
 
-		elfRuneButton = new JButton("ElfRune");
+		elfRuneButton = new JButton(new ImageIcon("textures//elfrunebutton.png"));
+		elfRuneButton.setBorderPainted(false);
+		elfRuneButton.setContentAreaFilled(false);
+		elfRuneButton.setFocusPainted(false);
+		elfRuneButton.setOpaque(false);
 		elfRuneButton.setActionCommand("elfrune");
 		elfRuneButton.addActionListener(control);
 		add(elfRuneButton);
 
-		manRuneButton = new JButton("ManRune");
+		manRuneButton = new JButton(new ImageIcon("textures//manrunebutton.png"));
+		manRuneButton.setBorderPainted(false);
+		manRuneButton.setContentAreaFilled(false);
+		manRuneButton.setFocusPainted(false);
+		manRuneButton.setOpaque(false);
 		manRuneButton.setActionCommand("manrune");
 		manRuneButton.addActionListener(control);
 		add(manRuneButton);
 
-		dwarfRuneButton = new JButton("DwarfRune");
+		dwarfRuneButton = new JButton(new ImageIcon("textures//dwarfrunebutton.png"));
+		dwarfRuneButton.setBorderPainted(false);
+		dwarfRuneButton.setContentAreaFilled(false);
+		dwarfRuneButton.setFocusPainted(false);
+		dwarfRuneButton.setOpaque(false);
 		dwarfRuneButton.setActionCommand("dwarfrune");
 		dwarfRuneButton.addActionListener(control);
 		add(dwarfRuneButton);
 
-		hobbitRuneButton = new JButton("HobbitRune");
+		hobbitRuneButton = new JButton(new ImageIcon("textures//hobbitrunebutton.png"));
+		hobbitRuneButton.setBorderPainted(false);
+		hobbitRuneButton.setContentAreaFilled(false);
+		hobbitRuneButton.setFocusPainted(false);
+		hobbitRuneButton.setOpaque(false);
 		hobbitRuneButton.setActionCommand("hobbitrune");
 		hobbitRuneButton.addActionListener(control);
 		add(hobbitRuneButton);
 
-		rangeRuneButton = new JButton("RangeRune");
+		rangeRuneButton = new JButton(new ImageIcon("textures//rangerunebutton.png"));
+		rangeRuneButton.setBorderPainted(false);
+		rangeRuneButton.setContentAreaFilled(false);
+		rangeRuneButton.setFocusPainted(false);
+		rangeRuneButton.setOpaque(false);
 		rangeRuneButton.setActionCommand("rangerune");
 		rangeRuneButton.addActionListener(control);
 		add(rangeRuneButton);
 
-		reloadRuneButton = new JButton("ReloadRune");
+		reloadRuneButton = new JButton(new ImageIcon("textures//reloadrunebutton.png"));
+		reloadRuneButton.setBorderPainted(false);
+		reloadRuneButton.setContentAreaFilled(false);
+		reloadRuneButton.setFocusPainted(false);
+		reloadRuneButton.setOpaque(false);
 		reloadRuneButton.setActionCommand("reloadrune");
 		reloadRuneButton.addActionListener(control);
 		add(reloadRuneButton);
