5deb129506572b1b96799ae10a8501391290308a
Martinek Vilmos <vilmos.martinek@gmail.com>
2014-05-11 20:37:06 +0200

    Fixed warning.

--- src/view/SidePanel.java
+++ src/view/SidePanel.java
@@ -6,25 +6,15 @@ import javax.swing.JPanel;
 
 import control.Control;
 
-public class SidePanel extends JPanel{
+public class SidePanel extends JPanel {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
-	MenuPanel menu;
-	StatusPanel status;
-	
-	
-	public SidePanel(StatusPanel status,Control control)
-	{
-		menu = new MenuPanel(control);
-		this.status = status;
 
+	public SidePanel(StatusPanel status, Control control) {
 		setLayout(new BorderLayout());
-		
-		add(menu,BorderLayout.CENTER);
-		add(status,BorderLayout.NORTH);
+
+		add(new MenuPanel(control), BorderLayout.CENTER);
+		add(status, BorderLayout.NORTH);
 	}
 
 }
