42517008744e547e85c4729a597f938be77adc75
Martinek Vilmos <vilmos.martinek@gmail.com>
2014-05-11 11:53:58 +0200

    Randomizer a little bit prettier

--- src/common/Randomizer.java
+++ src/common/Randomizer.java
@@ -4,18 +4,13 @@ import java.util.Random;
 
 public class Randomizer {
 
-	public static int randomInt(int max) {
-		Random r = new Random();
-		return r.nextInt(max);
+	private static Random random = new Random();
 
+	public static int randomInt(int max) {
+		return random.nextInt(max);
 	}
 
 	public static boolean randomBoolean(int percentage) {
-		Random r = new Random();
-		int number = r.nextInt(100);
-		if (number < percentage)
-			return true;
-		else
-			return false;
+		return random.nextInt(100) < percentage;
 	}
 }
