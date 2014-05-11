83c5109157e37cfce9d340014e00f68c532f1b59
m2kohand <uh.liamg@gmail.com>
2014-05-12 01:56:19 +0200

    bugfix, 20 live, 200 fogtimetoleave

--- src/common/Common.java
+++ src/common/Common.java
@@ -58,9 +58,9 @@ public class Common {
 
 	// Global values
 
-	public static final int life = 200;
+	public static final int life = 20;
 	public static final int damagePerUnit = 1;
-	public static final int fog_timetoleave = 20;
+	public static final int fog_timetoleave = 200;
 	public static final int fog_distancereduction = 1;
 	public static final int fog_simultaniousfognumber = 4;
 
